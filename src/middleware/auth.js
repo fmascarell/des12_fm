46a684de628780b854917c49d922994a15a45253
Francia Mascarell <fran.mascarell@hotmail.com>
2024-05-23 22:13:30 -0400

    upd

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,15 @@
 import { request, response } from "express";
 
+// Middleware de autenticación
 export const auth = (req = request, res = response, next) => {
   if (req.session?.user) return next();
 
   return res.redirect('/login');
+};
+
+// Middleware de autorización para administradores
+export const admin = (req = request, res = response, next) => {
+  if (req.session?.rol === 'admin') return next();
+
+  return res.redirect('/');
 };
--- src/routers/views.js
+++ src/routers/views.js
@@ -11,19 +11,32 @@ import {
   registerGet,
   registerPost,
 } from "../controllers/views.js";
-import { auth } from "../middleware/auth.js";
+import { admin, auth } from "../middleware/auth.js";
 
 const router = Router();
 
-router.get("/", auth, homeView);
-router.get("/realtimeproducts", auth, realtimeproductsView);
+// Ruta para la vista principal
+router.get("/", homeView);
+
+// Ruta para productos en tiempo real, requiere autenticación y permisos de administrador
+router.get("/realtimeproducts", [auth, admin], realtimeproductsView);
+
+// Ruta para el chat, requiere autenticación
 router.get("/chat", auth, chatView);
+
+// Ruta para ver productos, requiere autenticación
 router.get("/products", auth, productsView);
+
+// Ruta para ver el carrito, requiere autenticación
 router.get("/cart/:cid", auth, cartView);
+
+// Rutas para la autenticación (login y registro)
 router.get("/login", loginGet);
 router.post("/login", loginPost);
 router.get("/register", registerGet);
 router.post("/register", registerPost);
+
+// Ruta para cerrar sesión
 router.get("/logout", logout);
 
 export default router;
